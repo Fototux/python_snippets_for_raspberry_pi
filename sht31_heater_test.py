95c730a5c24caca1c38c1296fc9a46343467f9e4
Daniel Lehmann <github@fototux.com>
2021-06-05 12:34:15 +0200

    Add files via upload

---
+++ sht31_heater_test.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+import time
+from smbus2 import SMBus, i2c_msg
+
+# I2C bus 1 on a Raspberry Pi 3B+
+# SDA on GPIO2=Pin3 and SCL on GPIO3=Pin5
+# sensor +3V3 at Pin2 and GND at Pin6
+DEVICE_BUS = 1
+
+# device address SHT31
+DEVICE_ADDR = 0x44
+
+# init I2C
+bus = SMBus(DEVICE_BUS)
+
+#wait 1 s for sensor start up (> 1 ms according to datasheet)
+time.sleep(1)
+
+for _ in range(10):
+    # send measurement command for non periodic high repeatability to the sensor (0x2400)
+    msg = i2c_msg.write(DEVICE_ADDR, [0x24, 0x00])
+    bus.i2c_rdwr(msg)
+
+    # wait for measurement (15.5)
+    time.sleep(1)
+
+    # read 6 bytes; t: MSB, LSB, CRC; rh: MSB, LSB, CRC
+    msg = i2c_msg.read(DEVICE_ADDR, 6)
+    bus.i2c_rdwr(msg)
+
+    # merge byte 0 and byte 1 to int16_t
+    t_raw = msg.buf[0][0]<<8 | msg.buf[1][0]
+    rh_raw = msg.buf[3][0]<<8 | msg.buf[4][0]
+
+    # calculate according to datasheet section
+    t = -45 + 175 * (t_raw/65535)
+    rh = 100 * (rh_raw/65535)
+
+    print(("{0:.1f}\t{1:.1f}").format(t, rh))
+
+print ("enable heater")
+# enable heater
+msg = i2c_msg.write(DEVICE_ADDR, [0x30, 0x6D])
+bus.i2c_rdwr(msg)
+
+for _ in range(10):
+    # send measurement command for non periodic high repeatability to the sensor (0x2400)
+    msg = i2c_msg.write(DEVICE_ADDR, [0x24, 0x00])
+    bus.i2c_rdwr(msg)
+
+    # wait for measurement (15.5)
+    time.sleep(1)
+
+    # read 6 bytes; t: MSB, LSB, CRC; rh: MSB, LSB, CRC
+    msg = i2c_msg.read(DEVICE_ADDR, 6)
+    bus.i2c_rdwr(msg)
+
+    # merge byte 0 and byte 1 to int16_t
+    t_raw = msg.buf[0][0]<<8 | msg.buf[1][0]
+    rh_raw = msg.buf[3][0]<<8 | msg.buf[4][0]
+
+    # calculate according to datasheet section
+    t = -45 + 175 * (t_raw/65535)
+    rh = 100 * (rh_raw/65535)
+
+    print(("{0:.1f}\t{1:.1f}").format(t, rh))
+
+print ("status register")
+# read the status register
+msg = i2c_msg.write(DEVICE_ADDR, [0xF3, 0x2D])
+bus.i2c_rdwr(msg)
+# read 2 bytes
+msg = i2c_msg.read(DEVICE_ADDR, 6)
+bus.i2c_rdwr(msg)
+
+print (bin(msg.buf[0][0]<<8 | msg.buf[1][0])) 
+
+print ("perform soft reset")
+# soft reset
+msg = i2c_msg.write(DEVICE_ADDR, [0x30, 0xA2])
+bus.i2c_rdwr(msg)
+
+# wait > 1.5 ms to allow sensor to enter idle state again
+time.sleep(1)
+
+print ("status register")
+# read the status register
+msg = i2c_msg.write(DEVICE_ADDR, [0xF3, 0x2D])
+bus.i2c_rdwr(msg)
+# read 2 bytes
+msg = i2c_msg.read(DEVICE_ADDR, 6)
+bus.i2c_rdwr(msg)
+
+print (bin(msg.buf[0][0]<<8 | msg.buf[1][0])) 
+
+
+for _ in range(10):
+    # send measurement command for non periodic high repeatability to the sensor (0x2400)
+    msg = i2c_msg.write(DEVICE_ADDR, [0x24, 0x00])
+    bus.i2c_rdwr(msg)
+
+    # wait for measurement (15.5)
+    time.sleep(1)
+
+    # read 6 bytes; t: MSB, LSB, CRC; rh: MSB, LSB, CRC
+    msg = i2c_msg.read(DEVICE_ADDR, 6)
+    bus.i2c_rdwr(msg)
+
+    # merge byte 0 and byte 1 to int16_t
+    t_raw = msg.buf[0][0]<<8 | msg.buf[1][0]
+    rh_raw = msg.buf[3][0]<<8 | msg.buf[4][0]
+
+    # calculate according to datasheet section
+    t = -45 + 175 * (t_raw/65535)
+    rh = 100 * (rh_raw/65535)
+
+    print(("{0:.1f}\t{1:.1f}").format(t, rh))
+
+print ("disable heater")
+# disable heater
+msg = i2c_msg.write(DEVICE_ADDR, [0x30, 0x66])
+bus.i2c_rdwr(msg)
+
+for _ in range(10):
+    # send measurement command for non periodic high repeatability to the sensor (0x2400)
+    msg = i2c_msg.write(DEVICE_ADDR, [0x24, 0x00])
+    bus.i2c_rdwr(msg)
+
+    # wait for measurement (15.5)
+    time.sleep(1)
+
+    # read 6 bytes; t: MSB, LSB, CRC; rh: MSB, LSB, CRC
+    msg = i2c_msg.read(DEVICE_ADDR, 6)
+    bus.i2c_rdwr(msg)
+
+    # merge byte 0 and byte 1 to int16_t
+    t_raw = msg.buf[0][0]<<8 | msg.buf[1][0]
+    rh_raw = msg.buf[3][0]<<8 | msg.buf[4][0]
+
+    # calculate according to datasheet section
+    t = -45 + 175 * (t_raw/65535)
+    rh = 100 * (rh_raw/65535)
+
+    print(("{0:.1f}\t{1:.1f}").format(t, rh))
+
+
+bus.close()
---
+++ shtc3_smbus_example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import time
+from smbus2 import SMBus, i2c_msg
+
+# I2C bus 1 on a Raspberry Pi 3B+
+# SDA on GPIO2=Pin3 and SCL on GPIO3=Pin5
+# sensor +3V3 at Pin2 and GND at Pin6
+DEVICE_BUS = 1
+
+# device address SHTC3
+DEVICE_ADDR = 0x70
+
+# init I2C
+bus = SMBus(DEVICE_BUS)
+
+#wait 1 s for sensor start up (> 1 ms according to datasheet)
+time.sleep(1)
+
+for _ in range(10):
+    # send measurement command for non periodic high repeatability to the sensor (0x2400)
+    msg = i2c_msg.write(DEVICE_ADDR, [0x78, 0x66])
+    bus.i2c_rdwr(msg)
+
+    # wait for measurement
+    time.sleep(1)
+
+    # read 6 bytes; t: MSB, LSB, CRC; rh: MSB, LSB, CRC
+    msg = i2c_msg.read(DEVICE_ADDR, 6)
+    bus.i2c_rdwr(msg)
+
+    # merge byte 0 and byte 1 to int16_t
+    t_raw = msg.buf[0][0]<<8 | msg.buf[1][0]
+    rh_raw = msg.buf[3][0]<<8 | msg.buf[4][0]
+
+    # calculate according to datasheet section
+    t = -45 + 175 * (t_raw/65536)
+    rh = 100 * (rh_raw/65536)
+
+    print(("{0:.1f}\t{1:.1f}").format(t, rh))
+
+bus.close()
